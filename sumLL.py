9ac2d61d0b453fbca7acf5346c507c50d2c8d6b4
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2019-08-01 00:12:08 +0530

    rev

--- sumLL.py
+++ sumLL.py
@@ -9,51 +9,53 @@ Explanation: 342 + 465 = 807.
 '''
 
 class Node:
- def __init__(self,data):
-  self.data = data
-  self.next = None
+  def __init__(self,data):
+   self.data = data
+   self.next = None
   
 class LL:
- def __init__(self):
-  self.head = None
+  def __init__(self):
+   self.head = None
   
   
- def addEnd(self,data):
-  new = Node(data)
-  temp = self.head
-  if temp is None:
-   self.head = new
-   return
-  while(temp.next):
-   temp = temp.next
-  temp.next = new
- # recursively print nodes of a linked list
- def fprint(self,node):
-  if node is None:
-   return
-  print(node.data)
-  self.fprint(node.next)
-  
- def rprint(self,node):
-  if node is None:
-   return
-  self.rprint(node.next)
-  print(node.data)
+  def addEnd(self,data):
+   new = Node(data)
+   temp = self.head
+   if temp is None:
+    self.head = new
+    return
+   while(temp.next):
+    temp = temp.next
+   temp.next = new
+  # recursively print nodes of a linked list
+  def fprint(self,node):
+   if node is None:
+    return
+   print(node.data)
+   self.fprint(node.next) 
+  # recursively print nodes of a linked list in reverse order
+  def rprint(self,node):
+   if node is None:
+    return
+   self.rprint(node.next)
+   print(node.data)
   
 def revsum(node1,node2,ll3,carry):
- if node1 is None or node2 is None:
-  return ll3
- return revsum(node1.next,node2.next,ll3,carry)
- sumi = node1.data + node2.data + carry
- print("inter sum ",sumi)
- if sumi >= 10:
-  ll3.addEnd(sumi%10)
-  carry = sumi // 10
- else:
-  ll3.addEnd(sumi)
-  carry = 0
- print("inter list:")
- ll3.rprint(ll3.head)
+
+  if node1 is None or node2 is None:
+     return ll3
+  sumi = node1.data + node2.data + carry
+  #print("inter sum ",sumi)
+  if sumi >= 10:
+    ll3.addEnd(sumi%10)
+    carry = sumi // 10
+  else:
+    ll3.addEnd(sumi)
+    carry = 0
+  #print("inter list:")
+  #ll3.rprint(ll3.head)
+  return revsum(node1.next,node2.next,ll3,carry)
+ 
 if __name__== "__main__":
  ll = LL()
  ll2 = LL()
@@ -67,6 +69,6 @@ if __name__== "__main__":
  print("reverse sum:")
  ll3 = LL()
  f = revsum(ll.head,ll2.head,ll3,0)
- f.rprint(f.head)
+ f.fprint(f.head)
  #print("reverse print")
  #ll.rprint(ll.head)
